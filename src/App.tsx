c01c42f070bfec3de58cd6016ab38346abd68489
eusebiucroitor <119120965+eusebiucroitor@users.noreply.github.com>
2023-08-14 15:03:38 +0300

    Install Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Button, ButtonGroup } from '@chakra-ui/react'
 
 const App = () => {
   return (
     <div>
-      
+      <Button colorScheme='blue'>Button</Button>
     </div>
   )
 }
